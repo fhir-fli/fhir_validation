37aad6d5faca8adb85be178155fd56fd775c046a
Dokotela <grey.faulkenberry@mayjuun.com>
2024-05-17 19:45:23 -0400

    progress?

--- lib/fhir_validator_utils.dart
+++ lib/fhir_validator_utils.dart
@@ -19,7 +19,38 @@ class FhirValidatorUtils {
       throw Exception('Root node must be an ObjectNode');
     }
 
-    return await _objectNode(node, type, type, elements, results);
+    results = await _objectNode(node, type, type, elements, results);
+
+    // Check for missing required fields
+    results =
+        await _checkMissingRequiredFields(node, type, type, elements, results);
+
+    return results;
+  }
+
+  Future<ValidationResults> _checkMissingRequiredFields(
+    ObjectNode node,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
+    final currentPath = _cleanLocalPath(originalPath, replacePath, node.path);
+    for (final element in elements) {
+      if (element.min != null || element.max != null) {
+        final path = element.path;
+        if (path != null) {
+          Node? foundNode = node.children
+              .firstWhereOrNull((child) => child.path == currentPath);
+          if (foundNode == null && _isAPolymorphicElement(element)) {
+            foundNode = node.children.firstWhereOrNull(
+                (child) => child.path.replaceFirst('[x]', '') == currentPath);
+          }
+        }
+      }
+    }
+
+    return results;
   }
 
   List<ElementDefinition> extractElements(
@@ -28,30 +59,33 @@ class FhirValidatorUtils {
   }
 
   Future<ValidationResults> _traverseAst(
-      Node node,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
-    // print('traverse: ${node.path}');
+    Node node,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     if (node is ObjectNode) {
-      return _objectNode(node, originalPath, replacePath, elements, results);
+      return await _objectNode(
+          node, originalPath, replacePath, elements, results);
     } else if (node is ArrayNode) {
-      return _arrayNode(node, originalPath, replacePath, elements, results);
+      return await _arrayNode(
+          node, originalPath, replacePath, elements, results);
     } else if (node is PropertyNode) {
-      return _propertyNode(node, originalPath, replacePath, elements, results);
+      return await _propertyNode(
+          node, originalPath, replacePath, elements, results);
     } else {
       throw Exception('Invalid node type: ${node.runtimeType} at ${node.path}');
     }
   }
 
   Future<ValidationResults> _objectNode(
-      ObjectNode node,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
-    // print('object: ${node.path}');
+    ObjectNode node,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     for (var property in node.children) {
       results = await _propertyNode(
           property, originalPath, replacePath, elements, results);
@@ -60,12 +94,12 @@ class FhirValidatorUtils {
   }
 
   Future<ValidationResults> _arrayNode(
-      ArrayNode node,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
-    // print('array: ${node.path}');
+    ArrayNode node,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     for (final child in node.children) {
       if (child is LiteralNode) {
         final currentPath =
@@ -90,20 +124,17 @@ class FhirValidatorUtils {
   }
 
   Future<ValidationResults> _propertyNode(
-      PropertyNode node,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
-    // print('property: ${node.path}');
+    PropertyNode node,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     final cleanLocalPath =
         _cleanLocalPath(originalPath, replacePath, node.path);
     ElementDefinition? element =
         elements.firstWhereOrNull((element) => element.path == cleanLocalPath);
 
-    // If the element is null, but the key is "resourceType" and the value is
-    // a valid resourceType, then this is a valid entry, although it's not
-    // in the structure maps
     if (_isAResourceType(node, element)) {
       return results;
     }
@@ -113,29 +144,24 @@ class FhirValidatorUtils {
     if (element != null) {
       return await _withElement(
           node, element, originalPath, replacePath, elements, results);
-    }
-    // if we aren't able to find it in the elements, we should add it to the
-    // ValidationResults as an error, and won't go looking for anything else
-    else {
-      return results
-        ..addResult(
-          node,
-          'Element not found in StructureDefinition',
-          Severity.error,
-        );
+    } else {
+      results.addResult(
+        node,
+        'Element not found in StructureDefinition',
+        Severity.error,
+      );
+      return results;
     }
   }
 
   Future<ValidationResults> _withElement(
-      PropertyNode node,
-      ElementDefinition element,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
-    // We get the code from the StructureDefinition to determine if it's
-    // a primitive type
-
+    PropertyNode node,
+    ElementDefinition element,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     final String? code = _findCode(element, node.path);
     if (code != null) {
       return await _withCode(
@@ -147,13 +173,13 @@ class FhirValidatorUtils {
   }
 
   Future<ValidationResults> _withoutCode(
-      PropertyNode node,
-      ElementDefinition element,
-      String originalPath,
-      String replacePath,
-      ValidationResults results) async {
-    final extension = element.extension_;
-    for (final ext in extension ?? <FhirExtension>[]) {
+    PropertyNode node,
+    ElementDefinition element,
+    String originalPath,
+    String replacePath,
+    ValidationResults results,
+  ) async {
+    for (final ext in element.extension_ ?? <FhirExtension>[]) {
       final url = ext.url?.toString();
       if (url != null) {
         final structureDefinition = await getStructureDefinition(url);
@@ -165,22 +191,23 @@ class FhirValidatorUtils {
         }
       }
     }
-    return results
-      ..addResult(
-        node,
-        element.toJson().toString(),
-        Severity.error,
-      );
+    results.addResult(
+      node,
+      element.toJson().toString(),
+      Severity.error,
+    );
+    return results;
   }
 
   Future<ValidationResults> _withCode(
-      String code,
-      PropertyNode node,
-      ElementDefinition element,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
+    String code,
+    PropertyNode node,
+    ElementDefinition element,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     if (isPrimitiveType(code)) {
       return await _codeIsPrimitiveType(
           node, element, originalPath, replacePath, elements, results);
@@ -191,16 +218,14 @@ class FhirValidatorUtils {
   }
 
   Future<ValidationResults> _codeIsComplexType(
-      String code,
-      PropertyNode node,
-      ElementDefinition element,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
-    // If not, then it's not a primitive structure, and we will need
-    // to find a separate StructureDefinition or Profile to validate
-    // the object.
+    String code,
+    PropertyNode node,
+    ElementDefinition element,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     final structureDefinitionMap = await getStructureDefinition(code);
     final StructureDefinition? structureDefinition =
         structureDefinitionMap != null
@@ -210,7 +235,6 @@ class FhirValidatorUtils {
       return _noStructureDefinitionOrProfile(code, node, results);
     }
     final newElements = extractElements(structureDefinition);
-    // If we find the StructureDefinition, we should traverse the AST
     if (newElements.isNotEmpty) {
       if (node.value != null) {
         return await _traverseAst(
@@ -224,7 +248,10 @@ class FhirValidatorUtils {
   }
 
   ValidationResults _noStructureDefinitionOrProfile(
-          String code, PropertyNode node, ValidationResults results) =>
+    String code,
+    PropertyNode node,
+    ValidationResults results,
+  ) =>
       results
         ..addResult(
           node,
@@ -233,12 +260,13 @@ class FhirValidatorUtils {
         );
 
   Future<ValidationResults> _codeIsPrimitiveType(
-      PropertyNode node,
-      ElementDefinition element,
-      String originalPath,
-      String replacePath,
-      List<ElementDefinition> elements,
-      ValidationResults results) async {
+    PropertyNode node,
+    ElementDefinition element,
+    String originalPath,
+    String replacePath,
+    List<ElementDefinition> elements,
+    ValidationResults results,
+  ) async {
     if (node.value is LiteralNode) {
       return await _literalNode(node.value as LiteralNode, element, results);
     } else if (node.value is ArrayNode) {
@@ -250,21 +278,11 @@ class FhirValidatorUtils {
     }
   }
 
-  String _cleanLocalPath(
-      String originalPath, String replacePath, String childPath) {
-    childPath = childPath.replaceAll(originalPath, replacePath);
-    return _stripIndexes(childPath);
-  }
-
-  String _stripIndexes(String path) {
-    // Regular expression to match [index] patterns
-    RegExp regex = RegExp(r'\[\d+\]');
-    // Replace all matches with an empty string
-    return path.replaceAll(regex, '');
-  }
-
-  Future<ValidationResults> _literalNode(LiteralNode node,
-      ElementDefinition element, ValidationResults results) async {
+  Future<ValidationResults> _literalNode(
+    LiteralNode node,
+    ElementDefinition element,
+    ValidationResults results,
+  ) async {
     final primitiveClass = _findCode(element, node.path);
     final value = node.value;
     if (!isValueAValidPrimitive(primitiveClass.toString(), value)) {
@@ -291,8 +309,10 @@ class FhirValidatorUtils {
         path.startsWith(element.path!.replaceFirst('[x]', '')));
   }
 
+  bool _isAPolymorphicElement(ElementDefinition element) =>
+      element.path != null && element.path!.endsWith('[x]');
+
   String? _findCode(ElementDefinition element, String path) {
-    String? code;
     if (element.type?.length == 1) {
       return element.type?.first.code?.toString();
     } else if ((element.type?.length ?? 0) > 1) {
@@ -303,13 +323,23 @@ class FhirValidatorUtils {
             .replaceAll(
                 element.path?.split('.').last.replaceAll('[x]', '') ?? '', '')
             .toLowerCase();
-        code = element.type!
+        return element.type!
             .firstWhereOrNull((t) => t.code?.toString().toLowerCase() == type)
             ?.code
             ?.toString();
-        return code;
       }
     }
     return null;
+  }
+
+  String _cleanLocalPath(
+      String originalPath, String replacePath, String childPath) {
+    childPath = childPath.replaceAll(originalPath, replacePath);
+    return _stripIndexes(childPath);
+  }
+
+  String _stripIndexes(String path) {
+    RegExp regex = RegExp(r'\[\d+\]');
+    return path.replaceAll(regex, '');
   }
 }
