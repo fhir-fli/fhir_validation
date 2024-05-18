afc9ae24a5f94c6aae3b9da158359befa47f99e7
Dokotela <grey.faulkenberry@mayjuun.com>
2024-05-18 19:12:24 -0400

    next steps

--- lib/fhir_validator.dart
+++ lib/fhir_validator.dart
@@ -4,21 +4,12 @@ import 'package:fhir_r4/fhir_r4.dart';
 import 'package:fhir_validation/fhir_validation.dart';
 import 'package:http/http.dart';
 
-import 'fhir_validator_utils.dart';
-
 class FhirValidator {
-  var results = ValidationResults();
-
-  ValidationResults _results() {
-    final newResults = results.copyWith();
-    results = ValidationResults();
-    return newResults;
-  }
-
-  Future<ValidationResults> validateFhirResource(
-      {required Resource resourceToValidate,
-      StructureDefinition? structureDefinition,
-      Client? client}) {
+  Future<ValidationResults> validateFhirResource({
+    required Resource resourceToValidate,
+    StructureDefinition? structureDefinition,
+    Client? client,
+  }) {
     return validateFhirMap(
       resourceToValidate: resourceToValidate.toJson(),
       structureDefinition: structureDefinition,
@@ -45,12 +36,12 @@ class FhirValidator {
   }) async {
     final type = resourceToValidate['resourceType'] as String?;
     if (type == null) {
-      results.addResult(
-        null,
-        'ResourceType is missing',
-        Severity.error,
-      );
-      return _results();
+      return ValidationResults()
+        ..addResult(
+          null,
+          'ResourceType is missing',
+          Severity.error,
+        );
     }
     // PrettyPrinting makes lines and columns correct
     final String resourceString = prettyPrintJson(resourceToValidate);
@@ -58,39 +49,37 @@ class FhirValidator {
     // Parse the JSON with position information
     final node = parse(resourceString, Settings(), type);
 
-    final FhirValidatorUtils utils = FhirValidatorUtils();
-
     if (structureDefinition != null) {
       // If structure definition is found, validate the resource against it
-      results.combineResults(
-        await utils.evaluateFromPaths(
-          mapToValidate: resourceToValidate,
-          structureDefinition: structureDefinition,
-          type: type,
-          startPath: type,
-          node: node,
-        ),
+      return await evaluate(
+        resourceToValidate,
+        structureDefinition,
+        type,
+        type,
+        node,
+        client,
       );
-      return _results();
     }
 
     // Retrieve profiles for the resource
-    List<Map<String, dynamic>> profiles = await getProfiles(node, client);
+    List<Map<String, dynamic>> profiles = <Map<String, dynamic>>[];
+    final results = await _getProfiles(node, client, profiles);
 
     if (profiles.isNotEmpty) {
       // If profiles are found, validate the resource against each profile
       for (var profile in profiles) {
         results.combineResults(
-          await utils.evaluateFromPaths(
-            mapToValidate: resourceToValidate,
-            structureDefinition: StructureDefinition.fromJson(profile),
-            type: type,
-            startPath: type,
-            node: node,
+          await evaluate(
+            resourceToValidate,
+            StructureDefinition.fromJson(profile),
+            type,
+            type,
+            node,
+            client,
           ),
         );
       }
-      return _results();
+      return results;
     }
 
     // So we don't have profiles or a StructureDefinition, so we just go with
@@ -98,42 +87,72 @@ class FhirValidator {
     final definitionMap = await getStructureDefinition(type, client);
     if (definitionMap == null) {
       // If no structure definition is found, return an error
-      results.addResult(
-        null,
-        'No StructureDefinition was found for this Resource, which is a resourceType of: $type',
-        Severity.error,
-      );
-      return _results();
+      return results
+        ..addResult(
+          null,
+          'No StructureDefinition was found for this Resource, which is a resourceType of: $type',
+          Severity.error,
+        );
     } else {
       // Parse the structure definition from JSON
       structureDefinition = StructureDefinition.fromJson(definitionMap);
-      results.combineResults(
-        await utils.evaluateFromPaths(
-          mapToValidate: resourceToValidate,
-          structureDefinition: structureDefinition,
-          type: type,
-          startPath: type,
-          node: node,
-        ),
-      );
-      return _results();
+      return results
+        ..combineResults(
+          await evaluate(
+            resourceToValidate,
+            structureDefinition,
+            type,
+            type,
+            node,
+            client,
+          ),
+        );
     }
   }
 
-  Future<List<Map<String, dynamic>>> getProfiles(
-      Node node, Client? client) async {
-    // List to hold profile definitions retrieved
-    List<Map<String, dynamic>> profileDefinitions = [];
+  Future<ValidationResults> evaluate(
+    Map<String, dynamic> mapToValidate,
+    StructureDefinition structureDefinition,
+    String type,
+    String startPath,
+    Node node,
+    Client? client,
+  ) async {
+    var results = ValidationResults();
+    // Extract elements from the main structure definition
+    final elements = extractElements(structureDefinition);
+
+    results = await validateStructure(
+        node: node, elements: elements, type: type, client: client);
+
+    // Check for missing required fields and extra fields
+    results = await validateCardinality(structureDefinition.getUrl(),
+        node as ObjectNode, type, type, elements, results, client);
+
+    // Validate Coding/CodeableConcept bindings
+    results = await validateBindings(
+      node: node,
+      elements: elements,
+      results: results,
+      client: client,
+    );
+
+    return results;
+  }
+
+  Future<ValidationResults> _getProfiles(
+      Node node, Client? client, List<Map<String, dynamic>> profiles) async {
+    final ValidationResults results = ValidationResults();
     if (node is ObjectNode) {
       // Extract profiles from the resource if any are specified in the meta section
-      final profiles = node.extractProfileNodes();
+      final profileNodes = node.extractProfileNodes();
 
       // Retrieve profile definitions for each profile URL
-      for (var profile in profiles) {
+      for (var profile in profileNodes) {
         try {
           final profileDef = await getProfile(profile.value, client);
           if (profileDef != null) {
-            profileDefinitions.add(profileDef);
+            profiles.add(profileDef);
           }
         } catch (e) {
           // If there is an error retrieving a profile, return an error message
@@ -145,7 +164,6 @@ class FhirValidator {
         }
       }
     }
-
-    return profileDefinitions;
+    return results;
   }
 }
